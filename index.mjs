b07062c78c2a5380366a5b60cf7dc93741d5a5c3
bertiemoogle <bertiemoogle@gmail.com>
2023-02-19 20:06:41 +0000

    All queries in place.

--- index.mjs
+++ index.mjs
@@ -18,7 +18,41 @@ const queries = await inquirer
             name: "installation",
             message: "What is the method of installation?",
         },
-        
+        {
+            type: "input",
+            name: "usage",
+            message: "Are there any limits on usage?",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Please select the license you used for this project.",
+            choices: [
+              "MIT",
+              "Apache",
+              "Boost",
+            ],
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Is it possible to contribute to this project?",
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "What testing has been done on this project?",
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "Is it possible to contact you with questions about this project?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the URL for the Github repo?",
+        },
     ])
 
 
@@ -32,8 +66,19 @@ let readmeText =
 * Contributing
 * Tests
 * Questions
-## Installation:
+## Installation
 ${queries.installation}
-    `
+## Usage
+${queries.usage}
+## License
+${queries.license}
+## Contributing
+${queries.contributing}
+## Tests
+${queries.tests}
+## Questions
+${queries.questions}
+## Github
+${queries.githubURL}`
 
 fs.writeFile("generatedREADME.md", readmeText);
